ed334731db05107dcf7ac6f172dfaf34b4cc1da4
iCHIrTmA <jonrue.dev@gmail.com>
2024-12-08 17:07:27 +0800

    feat: sign out

--- client/components/AppNavigation.vue
+++ client/components/AppNavigation.vue
@@ -1,6 +1,11 @@
 <script setup lang="ts">
-const { isAuthenticated } = useSanctumAuth();
+const { isAuthenticated, logout: logoutAction } = useSanctumAuth();
 const user = useSanctumUser<User>()
+
+const logout = async () => {
+  await logoutAction()
+  await navigateTo('/example')
+}
 
 </script>
 <template>
@@ -17,6 +22,7 @@ const user = useSanctumUser<User>()
 
     <div v-if="isAuthenticated" class="flex items-center space-x-6">
       <div class="font-semibold">{{ user?.name }}</div>
+      <button v-on:click="logout">Sign out</button>
     </div>
   </div>
 </template>
